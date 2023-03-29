900b6338ecb6c95d8bfb1f8d103c7e37afeb1f46
urmila2801 <urmilapamarthi3@gmail.com>
2023-03-29 07:02:48 +0000

    clock-class

---
+++ src/components/Clock/index.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  state = {date: new Date()}
+
+  componentDidMount() {
+    console.log('componenent called')
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    console.log(date)
+
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
